d966b6e71865d192a07b6a47967475b869e1ab0f
DukeTranWM <dtran@email.wm.edu>
2020-08-20 14:21:47 -0400

    working on methods for State class (finished constructors, get/setValue, setEmptyBoard, addTile, isFull, canMerge, reachedThreshold). 5 unit tests passed.

--- Game2048ByDukeTran/src/edu/wm/cs/cs301/game2048/State.java
+++ Game2048ByDukeTran/src/edu/wm/cs/cs301/game2048/State.java
@@ -1,55 +1,89 @@
 package edu.wm.cs.cs301.game2048;
 
 public class State implements GameState {
+	
+	private int[] board;
 
 	public State() {
-		// TODO Auto-generated constructor stub
+		setEmptyBoard();
 	}
 	
 	public State(State currentState) {
-		// TODO Auto-generated constructor stub
+		board = currentState.getBoard();
+	}
+	
+	public int[] getBoard() {
+		return board;
 	}
 
 	@Override
 	public int getValue(int x, int y) {
-		// TODO Auto-generated method stub
-		return 0;
+		return board[(x * 4) + y];
 	}
 
 	@Override
 	public void setValue(int x, int y, int value) {
-		// TODO Auto-generated method stub
-
+		board[(x * 4) + y] = value;
 	}
 
 	@Override
 	public void setEmptyBoard() {
-		// TODO Auto-generated method stub
-
+		board = new int[16];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = 0;
+		}
 	}
 
 	@Override
 	public boolean addTile() {
-		// TODO Auto-generated method stub
-		return false;
+		if (isFull()) return false;
+		
+		int randomIdx;
+		do {
+			randomIdx = (int)(Math.random() * 16);
+		} while (board[randomIdx] != 0);
+		
+		double randomNum = Math.random();
+		board[randomIdx] = randomNum > 0.5 ? 2 : 4;
+		return true;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		for (int num: board) {
+			if (num == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public boolean canMerge() {
-		// TODO Auto-generated method stub
+		for (int row = 0; row < 4; row++) {
+			int compare = board[(row * 4)];
+			for (int col = 1; col < 4; col++) {
+				if (board[(row * 4) + col] == compare) {
+					return true;
+				} else if (board[(row * 4) + col] == 0) {
+					continue;
+				} else {	
+					compare = board[(row * 4) + col];
+				}
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public boolean reachedThreshold() {
-		// TODO Auto-generated method stub
-		return false;
+		int max = 0;
+		for (int num: board) {
+			if (num > max) {
+				max = num;
+			}
+		}
+		return max >= 2048 ? true : false;
 	}
 
 	@Override
