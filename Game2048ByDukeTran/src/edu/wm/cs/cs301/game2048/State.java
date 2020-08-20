2035538d2289591a0b5544f6c277d170c57f21ca
DukeTranWM <dtran@email.wm.edu>
2020-08-20 15:47:52 -0400

    keyboard functionality and scoring system successfully implemented, working on test cases

--- Game2048ByDukeTran/src/edu/wm/cs/cs301/game2048/State.java
+++ Game2048ByDukeTran/src/edu/wm/cs/cs301/game2048/State.java
@@ -1,5 +1,7 @@
 package edu.wm.cs.cs301.game2048;
 
+import java.util.Stack;
+
 public class State implements GameState {
 	
 	private int[] board;
@@ -18,12 +20,12 @@ public class State implements GameState {
 
 	@Override
 	public int getValue(int x, int y) {
-		return board[(x * 4) + y];
+		return board[(y * 4) + x];
 	}
 
 	@Override
 	public void setValue(int x, int y, int value) {
-		board[(x * 4) + y] = value;
+		board[(y * 4) + x] = value;
 	}
 
 	@Override
@@ -60,18 +62,34 @@ public class State implements GameState {
 
 	@Override
 	public boolean canMerge() {
-		for (int row = 0; row < 4; row++) {
-			int compare = board[(row * 4)];
-			for (int col = 1; col < 4; col++) {
-				if (board[(row * 4) + col] == compare) {
+		for (int x = 0; x < 4; x++) {
+			int compare = getValue(x, 0);
+			for (int y = 1; y < 4; y++) {
+				int tempValue = getValue(x, y); 
+				if (tempValue == compare) {
 					return true;
-				} else if (board[(row * 4) + col] == 0) {
+				} else if (tempValue == 0) {
 					continue;
 				} else {	
-					compare = board[(row * 4) + col];
+					compare = tempValue;
 				}
 			}
 		}
+		
+		for (int y = 0; y < 4; y++) {
+			int compare = getValue(0, y);
+			for (int x = 1; x < 4; x++) {
+				int tempValue = getValue(x, y); 
+				if (tempValue == compare) {
+					return true;
+				} else if (tempValue == 0) {
+					continue;
+				} else {	
+					compare = tempValue;
+				}
+			}
+		}
+		
 		return false;
 	}
 
@@ -88,26 +106,118 @@ public class State implements GameState {
 
 	@Override
 	public int left() {
-		// TODO Auto-generated method stub
-		return 0;
+		Stack<Integer> tileStack = new Stack<Integer>();
+		int score = 0;
+		
+		for (int y = 0; y < 4; y++) {
+			for (int x = 3; x >= 0; x--) {
+				int num = getValue(x, y);
+				if (num != 0) {
+					tileStack.push(num);
+					setValue(x, y, 0);
+				}
+			}
+			
+			int tempX = 0;
+			while (!tileStack.empty()) {
+				int tempNum = tileStack.pop();
+				if (!tileStack.empty() && tempNum == tileStack.peek()) {
+					tempNum += tileStack.pop(); 
+					score += tempNum;
+				}
+				setValue(tempX, y, tempNum);
+				tempX++;
+			}
+		}
+		
+		return score;
 	}
 
 	@Override
 	public int right() {
-		// TODO Auto-generated method stub
-		return 0;
+		Stack<Integer> tileStack = new Stack<Integer>();
+		int score = 0;
+		
+		for (int y = 0; y < 4; y++) {
+			for (int x = 0; x < 4; x++) {
+				int num = getValue(x, y);
+				if (num != 0) {
+					tileStack.push(num);
+					setValue(x, y, 0);
+				}
+			}
+			
+			int tempX = 3;
+			while (!tileStack.empty()) {
+				int tempNum = tileStack.pop();
+				if (!tileStack.empty() && tempNum == tileStack.peek()) {
+					tempNum += tileStack.pop(); 
+					score += tempNum;
+				}
+				setValue(tempX, y, tempNum);
+				tempX--;
+			}
+		}
+		
+		return score;
 	}
 
 	@Override
 	public int down() {
-		// TODO Auto-generated method stub
-		return 0;
+		Stack<Integer> tileStack = new Stack<Integer>();
+		int score = 0;
+		
+		for (int x = 0; x < 4; x++) {
+			for (int y = 0; y < 4; y++) {
+				int num = getValue(x, y);
+				if (num != 0) {
+					tileStack.push(num);
+					setValue(x, y, 0);
+				}
+			}
+			
+			int tempY = 3;
+			while (!tileStack.empty()) {
+				int tempNum = tileStack.pop();
+				if (!tileStack.empty() && tempNum == tileStack.peek()) {
+					tempNum += tileStack.pop();
+					score += tempNum;
+				}
+				setValue(x, tempY, tempNum);
+				tempY--;
+			}
+		}
+		
+		return score;
 	}
 
 	@Override
 	public int up() {
-		// TODO Auto-generated method stub
-		return 0;
+		Stack<Integer> tileStack = new Stack<Integer>();
+		int score = 0;
+		
+		for (int x = 0; x < 4; x++) {
+			for (int y = 3; y >= 0; y--) {
+				int num = getValue(x, y);
+				if (num != 0) {
+					tileStack.push(num);
+					setValue(x, y, 0);
+				}
+			}
+			
+			int tempY = 0;
+			while (!tileStack.empty()) {
+				int tempNum = tileStack.pop();
+				if (!tileStack.empty() && tempNum == tileStack.peek()) {
+					tempNum += tileStack.pop(); 
+					score += tempNum;
+				}
+				setValue(x, tempY, tempNum);
+				tempY++;
+			}
+		}
+		
+		return score;
 	}
 
 }
